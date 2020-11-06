9d95c368a6ace44ca47fa9fe90d643af46d13207
jinjh <869974640@qq.com>
2020-11-06 15:40:41 +0800

    ignore

---
+++ src/test/java/com/lmlx/currencymanagesystem/CurrencyManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmlx.currencymanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
