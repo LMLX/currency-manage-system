569b5f80682e52c9c082b5bf6d80bf71d9275f1f
jinjh <869974640@qq.com>
2020-11-06 15:34:06 +0800

    ignore

---
+++ src/main/java/com/lmlx/app/CurrencyManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lmlx.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyManageSystemApplication.class, args);
+    }
+
+}
